4cd51042c260ae50e04227e64268c319449f3113
artiscursive <kelson.fagan@gmail.com>
2022-01-14 09:10:41 -0600

    Update const.py

--- custom_components/nba/const.py
+++ custom_components/nba/const.py
@@ -1,5 +1,5 @@
 # API
-API_ENDPOINT = "http://site.api.espn.com/apis/site/v2/sports/football/nfl/scoreboard"
+API_ENDPOINT = "http://site.api.espn.com/apis/site/v2/sports/basketball/nba/scoreboard"
 USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15"
 
 # Config
@@ -8,14 +8,14 @@ CONF_TEAM_ID = "team_id"
 
 # Defaults
 DEFAULT_ICON = "mdi:football"
-DEFAULT_NAME = "NFL"
+DEFAULT_NAME = "NBA"
 DEFAULT_TIMEOUT = 120
 
 # Misc
 TEAM_ID = ""
 VERSION = "0.1"
 ISSUE_URL = "https://github.com/zacs/ha_nfl"
-DOMAIN = "nfl"
+DOMAIN = "nba"
 PLATFORM = "sensor"
 ATTRIBUTION = "Data provided by ESPN"
 COORDINATOR = "coordinator"
